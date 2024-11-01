f598645e7301cfc2e938866c2ed9a3a34878faeb
MartyJester <jurigrossi142@gmail.com>
2024-11-01 22:52:28 +0100

    Computes rough estimate

--- November2024.py
+++ November2024.py
@@ -1,9 +1,11 @@
 import numpy as np
 import random
-
-rolls = int(1e4)
+import matplotlib.pyplot as plt
+rolls = int(1e5)
 L = 1
 counter = 0
+track = []
+
 
 def compute_nearest(array):
     x, y = array[0], array[1]
@@ -14,14 +16,36 @@ def compute_nearest(array):
     return position[nearest_side_index]
 
 
+def area_in_target(position: str, red_dot: np.array, blue_dot: np.array):
+    if position == 'bottom':
+        sides = np.array([[0, 0], [1, 0]])
+    if position == 'left':
+        sides = np.array([[0, 0], [0, 1]])
+    if position == 'top':
+        sides = np.array([[0, 1], [1, 1]])
+    if position == 'right':
+        sides = np.array([[1, 0], [1, 1]])
+    distance_0 = np.linalg.norm(red_dot - sides[0])
+    distance_1 = np.linalg.norm(red_dot - sides[1])
+
+    distance_ref_0 = np.linalg.norm(blue_dot - sides[0])
+    distance_ref_1 = np.linalg.norm(blue_dot - sides[1])
+    if distance_0 <= distance_ref_0 or distance_1 <= distance_ref_1:
+        return 1
+    else:
+        return 0
 
 
-for i in range(50):
+for i in range(rolls):
+    print(i)
     blue = np.random.uniform(0, L, 2)
     red = np.random.uniform(0, L, 2)
     side = compute_nearest(blue)
-    print(side)
+    counter += area_in_target(side, red, blue)
+    track.append(counter)
 
-
-
-
+track = np.array(track)
+track = track / np.arange(1, len(track)+1)
+plt.plot(track[2:])
+plt.grid(True)
+plt.show()
